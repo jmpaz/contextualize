fa959fb6009abc5ed8aae45d97ca3e172d27cfe3
jmpaz <30947643+jmpaz@users.noreply.github.com>
2024-03-17 17:24:17 -0400

    Support `--format` and `--label` parameters in `cat`

--- contextualize/cli.py
+++ contextualize/cli.py
@@ -4,7 +4,7 @@ import argparse
 from contextualize.reference import FileReference, concat_refs
 
 
-def create_file_references(paths, ignore_paths=None):
+def create_file_references(paths, ignore_paths=None, format="md", label="relative"):
     file_references = []
     ignore_patterns = [
         # ".git/",
@@ -24,7 +24,7 @@ def create_file_references(paths, ignore_paths=None):
     for path in paths:
         if os.path.isfile(path):
             if not is_ignored(path, ignore_patterns):
-                file_references.append(FileReference(path))
+                file_references.append(FileReference(path, format=format, label=label))
         elif os.path.isdir(path):
             for root, dirs, files in os.walk(path):
                 dirs[:] = [
@@ -35,7 +35,9 @@ def create_file_references(paths, ignore_paths=None):
                 for file in files:
                     file_path = os.path.join(root, file)
                     if not is_ignored(file_path, ignore_patterns):
-                        file_references.append(FileReference(file_path))
+                        file_references.append(
+                            FileReference(file_path, format=format, label=label)
+                        )
 
     return file_references
 
@@ -46,7 +48,9 @@ def is_ignored(path, gitignore_patterns):
 
 
 def cat_cmd(args):
-    file_references = create_file_references(args.paths, args.ignore)
+    file_references = create_file_references(
+        args.paths, args.ignore, args.format, args.label
+    )
     concatenated_refs = concat_refs(file_references)
     print(concatenated_refs)
 
@@ -60,6 +64,16 @@ def main():
     )
     cat_parser.add_argument("paths", nargs="+", help="File or folder paths")
     cat_parser.add_argument("--ignore", nargs="*", help="File(s) to ignore")
+    cat_parser.add_argument(
+        "--format",
+        default="md",
+        help="Output format (options: 'md', 'xml', default 'md')",
+    )
+    cat_parser.add_argument(
+        "--label",
+        default="relative",
+        help="Label style (options: 'relative', 'name', 'ext', default 'relative')",
+    )
     cat_parser.set_defaults(func=cat_cmd)
 
     args = parser.parse_args()
